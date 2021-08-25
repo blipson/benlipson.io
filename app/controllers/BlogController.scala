d01ebdc34312545dd632d9ab79273a71e70c4953
Z003YW4 <ben.lipson@target.com>
2021-08-25 16:10:29 -0500

    Graphics History blog post

--- app/controllers/BlogController.scala
+++ app/controllers/BlogController.scala
@@ -6,11 +6,19 @@ import javax.inject.{Inject, Singleton}
 
 @Singleton
 class BlogController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+  def blog(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.blog())
+  }
+
   def onTravel(): Action[AnyContent] = Action { implicit  request: Request[AnyContent] =>
     Ok(views.html.onTravel())
   }
 
-  def blog(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.blog())
+  def onTechnology(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.onTechnology())
+  }
+
+  def graphicsHistory(): Action[AnyContent] = Action { implicit  request: Request[AnyContent] =>
+    Ok(views.html.graphicsHistory())
   }
 }
